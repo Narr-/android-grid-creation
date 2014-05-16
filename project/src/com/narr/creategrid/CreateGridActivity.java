24eca5d66987bd1cff92c2f65bca0e29521fe879
Narr Kim <nardgu@gmail.com>
2014-05-16 09:10:49 +0900

    add project

---
+++ project/src/com/narr/creategrid/CreateGridActivity.java
@@ -0,0 +1,19 @@
+package com.narr.creategrid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class CreateGridActivity extends Activity {
+
+	public static final String TAG = CreateGridActivity.class.getSimpleName();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.create_grid_activity_layout);
+
+		Log.d(TAG, "onCreate..!!");
+	}
+}
